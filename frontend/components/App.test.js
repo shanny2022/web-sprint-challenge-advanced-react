e1d06d4530be432a9dcd3acdb976c9306f20d3fc
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-20 22:14:05 -0500

    update

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,34 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('renders the grid', () => {
+  const { getByText } = render(<App />);
+  const gridElement = getByText(/B/i);
+  expect(gridElement).toBeInTheDocument();
+});
+
+test('renders the coordinates', () => {
+  const { getByText } = render(<App />);
+  const coordinatesElement = getByText(/Coordinates: 1, 1/i);
+  expect(coordinatesElement).toBeInTheDocument();
+});
+
+test('renders the steps', () => {
+  const { getByText } = render(<App />);
+  const stepsElement = getByText(/Steps: 0/i);
+  expect(stepsElement).toBeInTheDocument();
+});
+
+test('updates the input value when typing', () => {
+  const { getByLabelText } = render(<App />);
+  const inputElement = getByLabelText(/Email/i);
+  fireEvent.change(inputElement, { target: { value: 'test@example.com' } });
+  expect(inputElement.value).toBe('test@example.com');
+});
+
+test('renders the submit button', () => {
+  const { getByText } = render(<App />);
+  const buttonElement = getByText(/Submit/i);
+  expect(buttonElement).toBeInTheDocument();
+});
--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,88 +1,116 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-// Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialMessage = '';
+const initialEmail = '';
+const initialSteps = 0;
+const initialIndex = 4;
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
-
-export default class AppClass extends React.Component {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
-
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: initialMessage,
+      email: initialEmail,
+      steps: initialSteps,
+      index: initialIndex,
+    };
   }
 
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  getXY() {
+    const { index } = this.state;
+    const x = index % 3 + 1;
+    const y = Math.floor(index / 3) + 1;
+    return { x, y };
+  }
+
+  getXYMessage() {
+    const { x, y } = this.getXY();
+    return `Coordinates (${x}, ${y})`;
   }
 
   reset = () => {
-    // Use this helper to reset all states to their initial values.
+    this.setState({
+      message: initialMessage,
+      email: initialEmail,
+      steps: initialSteps,
+      index: initialIndex,
+    });
   }
 
-  getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+  getNextIndex(direction) {
+    let { index } = this.state;
+    switch (direction) {
+      case 'up':
+        if (index >= 3) index -= 3;
+        break;
+      case 'down':
+        if (index < 6) index += 3;
+        break;
+      case 'left':
+        if (index % 3 !== 0) index -= 1;
+        break;
+      case 'right':
+        if (index % 3 !== 2) index += 1;
+        break;
+    }
+    return index;
   }
 
   move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+    const newIndex = this.getNextIndex(evt.target.id);
+    if (newIndex !== this.state.index) {
+      this.setState(prevState => ({
+        index: newIndex,
+        steps: prevState.steps + 1,
+      }));
+    }
   }
 
   onChange = (evt) => {
-    // You will need this to update the value of the input.
+    this.setState({ email: evt.target.value });
   }
 
-  onSubmit = (evt) => {
-    // Use a POST request to send a payload to the server.
+  onSubmit = async (evt) => {
+    evt.preventDefault();
+    const { x, y } = this.getXY();
+    const { steps, email } = this.state;
+    const response = await axios.post('http://localhost:9000/api/result', { x, y, steps, email });
+    this.setState({ message: response.data.message });
   }
 
   render() {
-    const { className } = this.props
+    const { message, email, steps, index } = this.state;
     return (
-      <div id="wrapper" className={className}>
+      <div id="wrapper" className={this.props.className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{this.getXYMessage()}</h3>
+          <h3 id="steps">You moved {steps} times</h3>
         </div>
         <div id="grid">
-          {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
-              </div>
-            ))
-          }
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+              {idx === index ? 'B' : null}
+            </div>
+          ))}
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.move}>LEFT</button>
+          <button id="up" onClick={this.move}>UP</button>
+          <button id="right" onClick={this.move}>RIGHT</button>
+          <button id="down" onClick={this.move}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" placeholder="type email" value={email} onChange={this.onChange}></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
+
+export default AppClass;
